5b98fbfce8f7cf92feff2bafc637719ea7df102a
Alexander W Greene <greene.al1991@gmail.com>
2017-06-04 11:22:51 +0000

    Auth demo app

---
+++ README.md
@@ -0,0 +1,24 @@
+#Authentication
+
+##Intro to Authentication
+* What tools are we using?
+    * Passport
+    * Passport Local
+    * Passport Local Mongoose
+* Walk through auth flow
+* Discuss sessions
+    * Expired sessions
+
+##Auth CodeAlong part 1
+* Set up folder structure
+* Install needed packages
+* Add root route and template
+* Add secret route and template
+
+##Auth CodeAlong part 2
+* Create user model
+* Configure passport
+
+##Auth CodeAlong part 3
+* Add register routes
+* Add register form
---
+++ app.js
@@ -0,0 +1,77 @@
+var express                 = require("express"),
+    mongoose                = require("mongoose"),
+    passport                = require("passport"),
+    bodyParser              = require("body-parser"),
+    User                    = require("./models/user");
+    LocalStrategy           = require("passport-local"),
+    passportLocalMongoose   = require("passport-local-mongoose");
+    
+    
+mongoose.connect("mongodb://localhost/auth_demo_app");
+
+
+var app = express();
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(require("express-session")({
+    secret:"Rusty is the best dog ever",
+    resave: false,
+    saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+// =============================
+// ROUTES
+// =============================
+
+app.get("/", function(req, res){
+    res.render("home");
+});
+
+app.get("/register", function(req, res){
+   res.render("register");
+});
+
+// Show signup form
+app.post("/register", function(req, res){
+    console.log(req.body.password);
+    User.register(new User({username: req.body.username}), req.body.password, function(err, user){
+       if(err) {
+           console.log(err);
+           return res.render("register");
+       } else {
+            passport.authenticate("local")(req, res, function() {
+                res.redirect("/secret");
+            });
+       }
+   });
+});
+
+app.get("/login", function(req, res) {
+    res.render("login");
+});
+
+app.post("/login", passport.authenticate("local", {
+    successRedirect: "/secret",
+    failureRedirect: "/login"
+}), function(req, err) {
+});
+
+app.get("/secret", function(req, res) {
+    res.render("secret");
+})
+
+app.get("/logout", function(req, res) {
+    req.logout();
+    res.redirect("/");
+});
+
+app.listen(process.env.PORT, process.env.IP, function(){
+    console.log("SERVER STARTED!!!");
+});
